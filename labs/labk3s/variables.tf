baf796a72860a0a5128e8f54e43241803efcc9f9
Olivier BEAUTIER <raclepoulpe@gmail.com>
2022-11-22 17:29:33 +0000

    remove defaults values

--- labs/labk3s/variables.tf
+++ labs/labk3s/variables.tf
@@ -4,20 +4,14 @@ variable "keypairAdmin" {
 
 variable "imageId" {
  type = string
- default = "680bf266-52a2-4794-a3b7-9bb30a933bdc"
 }
 
 variable "flavorId" {
  type = string
- default = "906e8259-0340-4856-95b5-4ea2d26fe377"
 }
 
 variable "nameList" {
  type = list
  description = "List of instances"
- default = [
-"k3s001",
-"k3s002",
-"k3s003",
- ]
+ default = []
 }
