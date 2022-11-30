e87f95a4f5ad404a67eff3dfd7d9dc1015db09a2
Olivier BEAUTIER <raclepoulpe@gmail.com>
2022-11-30 16:13:22 +0000

    add inter vlan

--- networking/private-networks-multi-region/resources.tf
+++ networking/private-networks-multi-region/resources.tf
@@ -1,37 +1,91 @@
-resource "openstack_networking_network_v2" "network" {
+resource "openstack_networking_network_v2" "frontnetwork" {
   for_each		= { for o in var.z : o.region => o }
 
-  name           	= "${each.value.pvNetworkName}"
+  name           	= var.common.frontNwName
   region		= "${each.key}"
   admin_state_up 	= "true"
 }
 
-resource "openstack_networking_subnet_v2" "subnet" {
+resource "openstack_networking_subnet_v2" "frontsubnet" {
   for_each              = { for o in var.z : o.region => o }
 
-  network_id		= openstack_networking_network_v2.network[each.key].id
-  name                 	= "${each.value.subnetName}"
+  network_id		= openstack_networking_network_v2.frontnetwork[each.key].id
+  name                 	= var.common.frontSubnetName
   region               	= "${each.key}"
-  cidr                 	= "${each.value.subnetCIDR}"
+  cidr                 	= "${each.value.frontSubnetCIDR}"
   enable_dhcp          = false
   no_gateway           = false
   dns_nameservers      = ["1.1.1.1","1.0.0.1"]
 }
 
-resource "openstack_networking_router_v2" "router" {
+resource "openstack_networking_router_v2" "frontrouter" {
   for_each              = { for o in var.z : o.region => o }
 
   region		= "${each.key}"
-  name                  = "${each.value.routerName}"
+  name                  = var.common.frontRouterName
   admin_state_up        = true
   external_network_id   = "${data.openstack_networking_network_v2.Ext-Net[each.key].id}"
 }
 
-resource "openstack_networking_router_interface_v2" "routerInterface" {
+resource "openstack_networking_router_interface_v2" "frontRouterInterface" {
   for_each              = { for o in var.z : o.region => o }
 
   region                = "${each.key}"
-  router_id             = "${openstack_networking_router_v2.router[each.key].id}"
-  subnet_id             = "${openstack_networking_subnet_v2.subnet[each.key].id}"
+  router_id             = "${openstack_networking_router_v2.frontrouter[each.key].id}"
+  subnet_id             = "${openstack_networking_subnet_v2.frontsubnet[each.key].id}"
 }
 
+resource "ovh_cloud_project_network_private" "backnetwork" {
+  service_name 	= var.serviceName
+  name         	= var.common.backNwName
+  vlan_id	= var.common.backNwVlanId
+  regions      	= var.common.regions
+}
+
+resource "openstack_networking_subnet_v2" "backsubnet" {
+  for_each              = { for o in var.z : o.region => o }
+
+  network_id		= tolist(ovh_cloud_project_network_private.backnetwork.regions_attributes)[index(ovh_cloud_project_network_private.backnetwork.regions_attributes.*.region, each.key)].openstackid
+  region		= "${each.key}"
+  cidr                  = "${each.value.backSubnetCIDR}"
+  enable_dhcp          	= false
+  no_gateway           	= false
+  dns_nameservers      	= ["1.1.1.1","1.0.0.1"]
+}
+
+resource "openstack_networking_router_v2" "backrouter" {
+  for_each              = { for o in var.z : o.region => o }
+
+  region                = "${each.key}"
+  name                  = var.common.backRouterName
+  admin_state_up        = true
+}
+
+resource "openstack_networking_router_interface_v2" "backRouterInterfaceBack" {
+  for_each              = { for o in var.z : o.region => o }
+
+  region                = "${each.key}"
+  router_id             = "${openstack_networking_router_v2.backrouter[each.key].id}"
+  subnet_id             = "${openstack_networking_subnet_v2.backsubnet[each.key].id}"
+}
+
+resource "openstack_networking_port_v2" "backRouterPortFront" {
+  for_each              = { for o in var.z : o.region => o }
+
+  region		= "${each.key}"
+  name           	= var.common.portName
+  network_id     	= "${openstack_networking_network_v2.frontnetwork[each.key].id}"
+  admin_state_up 	= true
+  fixed_ip		{
+		subnet_id	= "${openstack_networking_subnet_v2.frontsubnet[each.key].id}"
+		ip_address	= "${each.value.backRouterFrontIP}"
+		}
+}
+
+resource "openstack_networking_router_interface_v2" "backRouterInterfaceFront" {
+  for_each              = { for o in var.z : o.region => o }
+
+  region                = "${each.key}"
+  router_id             = "${openstack_networking_router_v2.backrouter[each.key].id}"
+  port_id             	= "${openstack_networking_port_v2.backRouterPortFront[each.key].id}"
+}
--- networking/private-networks-multi-region/variables.tf
+++ networking/private-networks-multi-region/variables.tf
@@ -4,35 +4,61 @@ variable "serviceName" {
  type           = string
 }
 
+variable "common" {
+ type		= object({
+	regions			= list(string)
+	frontNwName		= string
+	frontSubnetName		= string
+	frontRouterName		= string
+	backNwName		= string
+	backNwVlanId		= number
+	backSubnetName		= string
+	backRouterName		= string
+	portName		= string
+	
+	})
+ default	= {
+	regions			= ["GRA7","SBG5","GRA9"]
+	frontNwName             = "frontNw"
+        frontSubnetName         = "frontSubnet"
+        frontRouterName         = "frontRouter"
+	backNwName		= "backNw"
+	backNwVlanId		= 100
+	backSubnetName		= "backSubnet"
+	backRouterName		= "backRouter"
+	portName		= "frontPort"
+	}
+}
+
 // Network regions parameters
 
 variable "z" {
  type		= list(object({
-	region		= string
-	pvNetworkName	= string
-	subnetName	= string
-	subnetCIDR      = string
-	routerName	= string
+      region		= string
+      frontSubnetCIDR	= string
+      backSubnetCIDR  	= string
+      backRouterFrontIP = string
+      backRouterBackIP	= string
 		}))
  default	= [
         {
-        	region          = "GRA7"
-        	pvNetworkName   = "nw1"
-		subnetName	= "sub1"
-		subnetCIDR	= "192.168.10.0/24"
-		routerName	= "rtr1"
+        	region          	= "GRA7"
+		frontSubnetCIDR		= "192.168.10.0/24"
+		backSubnetCIDR		= "192.168.110.0/24"
+		backRouterFrontIP	= "192.168.10.254"
+		backRouterBackIP	= "192.168.110.1"
         },{
-        	region          = "SBG5"
-        	pvNetworkName   = "nw2"
-		subnetName      = "sub2"
-		subnetCIDR      = "192.168.20.0/24"
-		routerName	= "rtr2"
+        	region          	= "SBG5"
+		frontSubnetCIDR		= "192.168.20.0/24"
+		backSubnetCIDR  	= "192.168.120.0/24"
+		backRouterFrontIP       = "192.168.20.254"
+                backRouterBackIP        = "192.168.120.1"
         },{
-                region          = "GRA9"
-                pvNetworkName   = "nw3"
-                subnetName      = "sub3"
-                subnetCIDR      = "192.168.30.0/24"
-                routerName      = "rtr3"
+                region          	= "GRA9"
+                frontSubnetCIDR		= "192.168.30.0/24"
+		backSubnetCIDR  	= "192.168.130.0/24"
+		backRouterFrontIP       = "192.168.30.254"
+                backRouterBackIP        = "192.168.130.1"
         }
    ]
 }
