98caa8db40e10f5dc3164ce703e1f275be12cea2
philippart-s <wildagsx@gmail.com>
2024-06-06 17:07:28 +0200

    feat: ✨ V1 for RAG & LangChain4j

--- ai/ai-endpoints/quarkus-langchain4j-rag/src/main/java/com/ovhcloud/examples/aiendpoints/services/ChatBotService.java
+++ ai/ai-endpoints/quarkus-langchain4j-rag/src/main/java/com/ovhcloud/examples/aiendpoints/services/ChatBotService.java
@@ -1,14 +1,5 @@
 package com.ovhcloud.examples.aiendpoints.services;
 
-import dev.langchain4j.service.SystemMessage;
-import dev.langchain4j.service.UserMessage;
-import io.quarkiverse.langchain4j.RegisterAiService;
-import io.smallrye.mutiny.Multi;
-
-@RegisterAiService
 public interface ChatBotService {
-
-  @SystemMessage("You are a cool AI assistant.")
-  @UserMessage("Answer as best possible to the following question: {question}. The answer must be in a style of a virtual assistant.")
-  Multi<String> askAQuestionStreamingMode(String question);
+  String answer(String query);
 }
