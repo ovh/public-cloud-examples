1ffbad0eaa40ff14e8b0252a78ee2397d13cea5d
philippart-s <wildagsx@gmail.com>
2024-06-07 15:29:47 +0200

    feat: ✨ clean code to keep only example code

--- ai/ai-endpoints/quarkus-langchain4j-rag/src/main/java/com/ovhcloud/examples/aiendpoints/services/ChatBotService.java
+++ ai/ai-endpoints/quarkus-langchain4j-rag/src/main/java/com/ovhcloud/examples/aiendpoints/services/ChatBotService.java
@@ -5,7 +5,7 @@ import dev.langchain4j.service.UserMessage;
 import io.quarkiverse.langchain4j.RegisterAiService;
 import io.smallrye.mutiny.Multi;
 
-@RegisterAiService(retrievalAugmentor = RetrievalAugmentorAIEndpoints.class)
+@RegisterAiService
 public interface ChatbotService {
   @SystemMessage("You are a cool AI assistant.")
   @UserMessage("Answer as best possible to the following question: {question}. The answer must be in a style of a virtual assistant.")
