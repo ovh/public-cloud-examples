475f98c12d9b8a6d9940d885b0e49e8342be142e
philippart-s <wildagsx@gmail.com>
2024-06-25 16:51:34 +0200

    feat: ✨ create java-langchain4j-chatbot project

---
+++ ai/ai-endpoints/java-langchain4j-chatbot/src/main/java/com/ovhcloud/examples/aiendpoints/App.java
@@ -0,0 +1,13 @@
+package com.ovhcloud.examples.aiendpoints;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
