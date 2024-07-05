976b839022897d26b10a3e181f76a765074805b2
philippart-s <wildagsx@gmail.com>
2024-07-05 12:08:34 +0200

    fix: 🐛 Remove absolute file name for RAG

--- ai/ai-endpoints/java-langchain4j-chatbot/src/main/java/com/ovhcloud/examples/aiendpoints/RAGStreamingChatbot.java
+++ ai/ai-endpoints/java-langchain4j-chatbot/src/main/java/com/ovhcloud/examples/aiendpoints/RAGStreamingChatbot.java
@@ -22,56 +22,56 @@ import dev.langchain4j.store.embedding.EmbeddingStore;
 import dev.langchain4j.store.embedding.inmemory.InMemoryEmbeddingStore;
 
 public class RAGStreamingChatbot {
-  private static final Logger _LOG = LoggerFactory.getLogger(RAGStreamingChatbot.class);
-  private static final String OVHCLOUD_API_KEY = System.getenv("OVHCLOUD_API_KEY");
+    private static final Logger _LOG = LoggerFactory.getLogger(RAGStreamingChatbot.class);
+    private static final String OVHCLOUD_API_KEY = System.getenv("OVHCLOUD_API_KEY");
 
-  interface Assistant {
-    TokenStream chat(String userMessage);
-  }
+    interface Assistant {
+        TokenStream chat(String userMessage);
+    }
 
-  public static void main(String[] args) {
-    DocumentParser documentParser = new TextDocumentParser();
-    Document document = loadDocument(
-        "/Users/sphilipp/dev/ovh/corp/public-cloud-examples/ai/ai-endpoints/java-langchain4j-chatbot/src/main/resources/rag-files/content.txt",
-        documentParser);
+    public static void main(String[] args) {
+        DocumentParser documentParser = new TextDocumentParser();;
+        Document document = loadDocument(
+                RAGStreamingChatbot.class.getResource("/rag-files/content.txt").getFile(),
+                documentParser);
 
 
-    DocumentSplitter splitter = DocumentSplitters.recursive(300, 0);
-    List<TextSegment> segments =
-        splitter.split(document);
+        DocumentSplitter splitter = DocumentSplitters.recursive(300, 0);
+        List<TextSegment> segments =
+                splitter.split(document);
 
-    EmbeddingModel embeddingModel = new BgeSmallEnV15QuantizedEmbeddingModel();
-    List<Embedding> embeddings = embeddingModel.embedAll(segments).content();
+        EmbeddingModel embeddingModel = new BgeSmallEnV15QuantizedEmbeddingModel();
+        List<Embedding> embeddings = embeddingModel.embedAll(segments).content();
 
-    EmbeddingStore<TextSegment> embeddingStore = new InMemoryEmbeddingStore<>();
-    embeddingStore.addAll(embeddings, segments);
+        EmbeddingStore<TextSegment> embeddingStore = new InMemoryEmbeddingStore<>();
+        embeddingStore.addAll(embeddings, segments);
 
-    ContentRetriever contentRetriever = EmbeddingStoreContentRetriever.builder()
-        .embeddingStore(embeddingStore)
-        .embeddingModel(embeddingModel)
-        .maxResults(2)
-        .minScore(0.5)
-        .build();
+        ContentRetriever contentRetriever = EmbeddingStoreContentRetriever.builder()
+                .embeddingStore(embeddingStore)
+                .embeddingModel(embeddingModel)
+                .maxResults(2)
+                .minScore(0.5)
+                .build();
 
-    MistralAiStreamingChatModel streamingChatModel = MistralAiStreamingChatModel.builder()
-        .apiKey(OVHCLOUD_API_KEY).modelName("Mixtral-8x22B-Instruct-v0.1")
-        .baseUrl(
-            "https://mixtral-8x22b-instruct-v01.endpoints.kepler.ai.cloud.ovh.net/api/openai_compat/v1/")
-        .maxTokens(1500).build();
+        MistralAiStreamingChatModel streamingChatModel = MistralAiStreamingChatModel.builder()
+                .apiKey(OVHCLOUD_API_KEY).modelName("Mixtral-8x22B-Instruct-v0.1")
+                .baseUrl(
+                        "https://mixtral-8x22b-instruct-v01.endpoints.kepler.ai.cloud.ovh.net/api/openai_compat/v1/")
+                .maxTokens(1500).build();
 
-    Assistant assistant =
-        AiServices.builder(Assistant.class)
-            .streamingChatLanguageModel(streamingChatModel)
-            .contentRetriever(contentRetriever)
-            .build();
+        Assistant assistant =
+                AiServices.builder(Assistant.class)
+                        .streamingChatLanguageModel(streamingChatModel)
+                        .contentRetriever(contentRetriever)
+                        .build();
 
-    _LOG.info("\n💬: What is AI Endpoints?.\n");
-    TokenStream tokenStream = assistant.chat("What is AI Endpoints?");
-    _LOG.info("🤖: ");
-    tokenStream
-        .onNext(_LOG::info)
-        .onError(Throwable::printStackTrace)
-        .start();
+        _LOG.info("\n💬: What is AI Endpoints?.\n");
+        TokenStream tokenStream = assistant.chat("What is AI Endpoints?");
+        _LOG.info("🤖: ");
+        tokenStream
+                .onNext(_LOG::info)
+                .onError(Throwable::printStackTrace)
+                .start();
 
-  }
+    }
 }
