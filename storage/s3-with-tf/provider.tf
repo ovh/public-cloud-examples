f610eff5b9e444ee81c9f39593929cdcac5cb1fd
Aurelie Vache <scraly@gmail.com>
2025-03-27 14:45:25 +0000

    init

--- storage/s3-with-tf/provider.tf
+++ storage/s3-with-tf/provider.tf
@@ -1,21 +1,11 @@
 terraform {
   required_providers {
-    aws = {
-      source  = "hashicorp/aws"
+    ovh = {
+      source  = "ovh/ovh"
     }
   }
 }
 
-# Configure the AWS Provider
-provider "aws" {
-  region     = "${var.region}"
-
-  # OVH implementation has no STS service
-  skip_credentials_validation = true
-  skip_requesting_account_id  = true
-  # the gra region is unknown to AWS hence skipping is needed.
-  skip_region_validation = true
-  endpoints {
-    s3 = "https://s3.${var.region}.io.cloud.ovh.net"
-  }
+provider "ovh" {
+  endpoint           = "ovh-eu"
 }
--- storage/s3-with-tf/variables.tf
+++
@@ -1,3 +0,0 @@
-variable "region" {
-    default = "gra"
-}
